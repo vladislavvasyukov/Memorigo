99e2176f7ff30f100170dedfe249868281acf734
Vladislav <vladislav.vasyukof@gmail.com>
2018-10-07 18:25:40 +0300

    add handler for registration

--- registration/registration.cpp
+++ registration/registration.cpp
@@ -1,5 +1,6 @@
 #include "registration.h"
 #include "ui_registration.h"
+#include <QMessageBox>
 
 Registration::Registration(QWidget *parent) :
     QDialog(parent),
@@ -20,5 +21,24 @@ void Registration::on_cancelButton_clicked()
 
 void Registration::on_registrationButton_clicked()
 {
+    QMessageBox successMsg;
+    successMsg.setText("Information");
+    successMsg.setInformativeText("Поздравляем! Вы успешно зарегистрировались!");
+    successMsg.setIcon(QMessageBox::Information);
 
+    QMessageBox failMsg;
+    failMsg.setText("Error");
+    failMsg.setInformativeText("Неправильное подтверждение пароля! Попробуйте ещё раз.");
+    failMsg.setIcon(QMessageBox::Information);
+
+    bool isOpen = ui->password->text() == ui->repeatPassword->text();
+
+    if (isOpen){
+        int res = successMsg.exec();
+        if (res == QMessageBox::Ok){
+            this->close();
+        }
+    } else {
+        int res = failMsg.exec();
+    }
 }
